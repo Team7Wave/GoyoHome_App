626225cdddfe29705757c7dba8860e847458a70a
seongsu0 <gmdstocker@naver.com>
2025-04-10 18:14:10 +0900

    AccountSettingActivity.java, activity_accountsetting.xml 계정 설정 화면

---
+++ app/src/main/java/com/example/goyohome_app/AccountSettingActivity.java
@@ -0,0 +1,13 @@
+package com.example.goyohome_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccountSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accountsetting);
+    }
+}
