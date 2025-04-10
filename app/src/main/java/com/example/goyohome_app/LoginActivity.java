c5ab316b972179fbe3716304e3de5f72e3ca9445
seongsu0 <gmdstocker@naver.com>
2025-04-10 17:01:22 +0900

    LoginActivity.java, activity_login.xml, fade_in.xml(anim) 로그인 페이지

---
+++ app/src/main/java/com/example/goyohome_app/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.goyohome_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ImageView logo = findViewById(R.id.logo);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.startAnimation(fadeIn);
+    }
+}
